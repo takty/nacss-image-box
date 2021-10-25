191796216d3d57f0b5081a287140293219c4d17e
Takuto Yanagida <yanagida@stxst.com>
2021-10-25 13:13:45 +0900

    Add common functions

--- src/js/_utilities.js
+++ src/js/_utilities.js
@@ -3,7 +3,7 @@
  * Utilities
  *
  * @author Takuto Yanagida
- * @version 2021-10-18
+ * @version 2021-10-25
  *
  */
 
@@ -25,6 +25,23 @@ function enableClass(enabled, tar, cls) {
 
 	}
 }
+
+function getSelector(cls) {
+	if (cls.startsWith(':')) {
+		return `*[data-${cls.substr(1).replace(/([A-Z])/g, c => '-' + c.charAt(0).toLowerCase())}]`;
+	} else {
+		return `*${cls}`;
+	}
+}
+
+function getScrollOffset() {
+	const s = getComputedStyle(document.getElementsByTagName('html')[0]);
+	return parseInt(s.scrollPaddingTop);
+}
+
+
+// -----------------------------------------------------------------------------
+
 
 function throttle(fn) {
 	let isRunning;
