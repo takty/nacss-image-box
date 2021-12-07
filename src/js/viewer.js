e56e211ae8e927f87d680f244f333b0d063602d4
Takuto Yanagida <yanagida@stxst.com>
2021-12-07 23:28:02 +0900

    Refactoring

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -3,7 +3,7 @@
  * Viewer
  *
  * @author Takuto Yanagida
- * @version 2021-11-19
+ * @version 2021-12-07
  *
  */
 
@@ -13,21 +13,21 @@ window['NACSS'] = window['NACSS'] ?? {};
 
 (function (NS) {
 
-	(function () {
+	{
 		// @include _touch.js
 		// @include _image.js
 		NS.viewerImage  = initializeImageViewer;
-	})();
+	}
 
-	(function () {
+	{
 		// @include _iframe.js
 		NS.viewerIframe = initializeIframeViewer;
-	})();
+	}
 
 	// @include _dialog.js
-
 	// @include _hash.js
+
 	// @include _style-class.js
-	// @include _utilities.js
+	// @include _utility.js
 
 })(window['NACSS']);
